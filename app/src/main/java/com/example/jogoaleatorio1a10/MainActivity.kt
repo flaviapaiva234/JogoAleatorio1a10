b7f6157ee7fe9811ce8d31de53b86163926cd0fb
flavia <flaviapaiva234@gmail.com>
2023-02-13 06:22:33 -0300

    second commit

---
+++ app/src/main/java/com/example/jogoaleatorio1a10/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.jogoaleatorio1a10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sortearNumero(view: View) {
+         var texto = findViewById(R.id.tv2textoSorteio) as TextView
+          texto.setText("Texto Alterado")
+    }
+
+}
